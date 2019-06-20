f0222b222aaaa8209f7637938b9d2d7b7c0bac3d
Yutong Xie <xieyutong78@gmail.com>
2019-06-20 10:40:59 -0400

    hardware

--- hardware/setbluetooth/setbluetooth.ino
+++ hardware/setbluetooth/setbluetooth.ino
@@ -1,4 +1,4 @@
-#define AT 2//en
+#define AT 12//en
 #define LED 13
 
 void setup()
@@ -10,11 +10,11 @@ void setup()
   delay(100);
   Serial.println("AT");
   delay(100);
-  Serial.println("AT+NAME=Cat Feeder");//命名模块名
+  Serial.println("AT+NAME=Cat-Feeder");//命名模块名
   delay(100);
   Serial.println("AT+ROLE=0");//设置主从模式：0从机，1主机
   delay(100);
-  //Serial.println("AT+PSWD=1234");//设置配对密码，如1234
+  Serial.println("AT+PSWD=1234");//设置配对密码，如1234
   delay(100);
   Serial.println("AT+UART=115200,0,0");//设置波特率9600，停止位1，校验位无
   delay(100);
---
+++ hardware/test/main/main.ino
@@ -0,0 +1,54 @@
+#include <SD.h>
+#include <LiquidCrystal_I2C.h>
+#include <Stepper.h>
+#include <Q2HX711.h>
+#include <SPI.h>
+
+
+File myFile;
+Q2HX711 scale1(3, 2);
+Q2HX711 scale2(7, 6);
+LiquidCrystal_I2C lcd(0x27,16,2);  
+#define spr=200;
+Stepper motor(200,A0,A1,A2,A3);
+
+void setup() {
+  lcd.init();
+  lcd.backlight(); 
+  Serial.begin(115200);
+  motor.setSpeed(60);
+  Serial.println("connected");
+ 
+  /*
+  while(1){
+    if (!SD.begin()) {
+      lcd.setCursor(0,0);                //设置显示指针
+      lcd.print("SD Init Fail");
+      continue;
+    }
+    lcd.setCursor(0,0);                //设置显示指针
+    lcd.print("SD card detected");
+    break;
+  }
+  myFile = SD.open("test.txt", FILE_WRITE);
+  myFile.seek(myFile.size());
+  myFile.write("this is my data");
+  myFile.close();
+ */
+}
+
+void loop() {
+  if(!Serial.available()) return;
+  char letter=Serial.read();
+  Serial.write(letter);
+  if(letter=="x"){
+    Serial.println("y");
+  }
+  if(letter=="a"){
+    Serial.println("b");
+  }
+  motor.setSpeed(100);
+  //motor.step(40);
+  //Serial.println("s1"+String(scale1.read()/100000-70));
+  //Serial.println("s2"+String(scale2.read()/10000));
+}
